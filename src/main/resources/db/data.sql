125105cd8bcbe2bf752efd4ceb0c578dd44e2d0d
Baek Junha <jooha208@gmail.com>
2021-07-16 16:59:50 +0900

    add schema

---
+++ src/main/resources/db/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO item(item_id, price, name) values ( 1, 300, 'jpa book' );
+INSERT INTO member(member_id, address, name) values ( 1, 'seoul', 'junha');
+INSERT INTO orders(order_id, qty, total_price, member_id, item_id) values ( 1, 3, 900, 1, 1);
